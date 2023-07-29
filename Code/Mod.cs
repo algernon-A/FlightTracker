524bd57a1a7da28542765713cda0e18a5bfce25a
algernon-A <aeneas@aloku.net>
2023-07-29 21:37:24 +1000

    Add project files.

---
+++ Code/Mod.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="Mod.cs" company="algernon (K. Algernon A. Sheppard)">
+// Copyright (c) algernon (K. Algernon A. Sheppard). All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace FlightTracker
+{
+    /// <summary>
+    /// The base mod class for instantiation by the game.
+    /// </summary>
+    public sealed class Mod
+    {
+    }
+}
